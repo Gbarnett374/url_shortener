bb7d2f8f0c1d8db9c0c7fd84d5b3f4de1a2fe829
Gbarnett374 <Gbarnett374@gmail.com>
2018-04-21 11:18:28 -0400

    Have controler and model generated for url.

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,10 @@
+class UrlsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'urls#index'
+  
+  get 'urls/index'
+
+  get 'urls/create'
+
+  get 'urls/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
